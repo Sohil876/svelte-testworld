f4f29056896e4656ed34c79a834ce26bbe9b6b2f
Sohil876 <sohil876@gmail.com>
2023-10-23 22:25:24 +0530

    Use our fake end point to fetch data and render list of fake random users on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
+<script>
+	export let data;
+</script>
+
 <main>
-	<h1>HOME</h1>
+	{#each data.users as { avatar, lastName }}
+		<a href={`/${lastName}`} class="box">
+			<img src={avatar} alt={lastName} />
+			<h2>{lastName}</h2>
+		</a>
+	{/each}
 </main>
+
+<style>
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+	.box {
+		padding: 0.25rem;
+		margin: 1.5rem;
+		color: salmon;
+		box-shadow: 4px 5px 11px 2px lightgray;
+	}
+	.box:hover {
+		box-shadow: 4px 5px 11px 10px lightgray;
+	}
+	img {
+		width: 15rem;
+		object-fit: contain;
+	}
+	.box h2 {
+		text-align: center;
+	}
+</style>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,12 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 // export const prerender = true;
+
+import { error } from '@sveltejs/kit';
+
+export async function load({ fetch }) {
+	const res = await fetch('/api');
+
+	if (res.ok) return { users: await res.json() };
+	throw error(500);
+}
